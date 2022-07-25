7b573061574299d840f4a10af3b1f575a534a54d
ST0PHoon <codingnb306@naver.com>
2022-07-25 10:25:52 +0900

    원글, 댓글 controller 분리

--- src/main/java/kr/ac/kopo/ctc/kopo37/controller/ArticleBoardController.java
+++ src/main/java/kr/ac/kopo/ctc/kopo37/controller/ArticleBoardController.java
@@ -1,17 +1,13 @@
 package kr.ac.kopo.ctc.kopo37.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.ac.kopo.ctc.kopo37.domain.ArticleBoard;
-import kr.ac.kopo.ctc.kopo37.domain.ArticleReply;
 import kr.ac.kopo.ctc.kopo37.service.ArticleBoardService;
 import kr.ac.kopo.ctc.kopo37.service.ArticleReplyService;
 
@@ -113,50 +109,5 @@ public class ArticleBoardController {
 		
 		return "redirect:/articleBoard/articleListIndex";
 	}
-	
-	
-	
-	// 댓글
-	@RequestMapping(value = "/createArticleReply")
-	public String createArticleReply(ArticleReply articleReply) {
-		articleReplyService.createArticleReply(articleReply);
-		
-		Long toGO = articleReply.getReplyId();
-		return "redirect:/articleBoard/selectedArticle/" + toGO;
-	}
-	
-	// 댓글
-	@RequestMapping(value = "/createArticleReplyReply")
-	public String createArticleReplyReply(ArticleReply articleReply) {
-		articleReplyService.createArticleReply(articleReply);
-		
-		Long toGO = articleReply.getReplyId();
-		return "redirect:/articleBoard/selectedArticle/" + toGO;
-	}
-	
-	@RequestMapping(value = "/articleReplyUpdateForm/{id}")
-	public String updateArticleReplyForm(Model model, @PathVariable("id") Long id) {
-		model.addAttribute("updateArticleReply", articleReplyService.findById(id));
-		
-		return "articleReplyUpdateForm";
-	}
-	
-	@RequestMapping(value = "/articleReplyUpdateForm/articleReplyUpdate")
-	public String updateArticleReply(ArticleReply articleReply) {
-		
-		articleReplyService.updateArticleReply(articleReply);
-
-		return "redirect:/articleBoard/articleListIndex";
-	}
-	
-	@RequestMapping(value = "/deleteArticleReply/{id}")
-	public String deleteArticleReply( @PathVariable("id") Long id) {
-		
-		articleReplyService.deleteById(id);
-		
-		return "redirect:/articleBoard/articleListIndex";
-	}
-	
-	
 	
 }
--- src/main/java/kr/ac/kopo/ctc/kopo37/controller/ArticleReplyController.java
+++ src/main/java/kr/ac/kopo/ctc/kopo37/controller/ArticleReplyController.java
@@ -6,14 +6,55 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.ac.kopo.ctc.kopo37.domain.ArticleReply;
 import kr.ac.kopo.ctc.kopo37.service.ArticleBoardService;
 import kr.ac.kopo.ctc.kopo37.service.ArticleReplyService;
 
 @Controller
-@RequestMapping(value = "/articleReply")
+@RequestMapping(value = "/articleBoard")
 public class ArticleReplyController {
 	@Autowired
 	private ArticleReplyService articleReplyService;
+	
+	// 댓글
+	@RequestMapping(value = "/createArticleReply")
+	public String createArticleReply(ArticleReply articleReply) {
+		articleReplyService.createArticleReply(articleReply);
+		
+		Long toGO = articleReply.getReplyId();
+		return "redirect:/articleBoard/selectedArticle/" + toGO;
+	}
+	
+	// 댓글
+	@RequestMapping(value = "/createArticleReplyReply")
+	public String createArticleReplyReply(ArticleReply articleReply) {
+		articleReplyService.createArticleReply(articleReply);
+		
+		Long toGO = articleReply.getReplyId();
+		return "redirect:/articleBoard/selectedArticle/" + toGO;
+	}
+	
+	@RequestMapping(value = "/articleReplyUpdateForm/{id}")
+	public String updateArticleReplyForm(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("updateArticleReply", articleReplyService.findById(id));
+		
+		return "articleReplyUpdateForm";
+	}
+	
+	@RequestMapping(value = "/articleReplyUpdateForm/articleReplyUpdate")
+	public String updateArticleReply(ArticleReply articleReply) {
+		
+		articleReplyService.updateArticleReply(articleReply);
 
+		return "redirect:/articleBoard/articleListIndex";
+	}
+	
+	@RequestMapping(value = "/deleteArticleReply/{id}")
+	public String deleteArticleReply( @PathVariable("id") Long id) {
+		
+		articleReplyService.deleteById(id);
+		
+		return "redirect:/articleBoard/articleListIndex";
+	}
 
 }
